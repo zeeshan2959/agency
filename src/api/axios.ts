0c7a7992043730f420c920310842d6e8f5edcb6b
Muhammad Zeeshan <muhammadzeeshan4902959@gmail.com>
2025-07-31 04:10:01 +0500

    content type added in axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,6 +4,9 @@ const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
     baseURL: API_URL,
     withCredentials: true, // for CSRF cookies
+     headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default api;
